cdbcf567dc73cd44cf2393b9546dbf0902a31359
dkarm <dkarmondy@gmail.com>
2025-01-26 21:56:01 -0700

    Changed password to user secrets

--- obj/Debug/net9.0/WatchList.AssemblyInfo.cs
+++ obj/Debug/net9.0/WatchList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7ccaef9ddc8d897d4a8839020fa89c76e398f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c46c43c2d53e0919abaf65b358f33afaccb6cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
